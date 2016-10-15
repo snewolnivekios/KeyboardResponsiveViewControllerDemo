b453d35bbcf27130d773135f6d900b759fbc7430
snewolk <snewolk@kevinlowens.com>
2016-10-15 07:20:52 -0500

    Adds storyboard views

--- KeyboardResponsiveDemo/AppDelegate.swift
+++ KeyboardResponsiveDemo/AppDelegate.swift
@@ -2,9 +2,22 @@
 //  AppDelegate.swift
 //  KeyboardResponsiveDemo
 //
-//  Created by Kevin Owens on 10/15/16.
 //  Copyright © 2016 Kevin L. Owens. All rights reserved.
 //
+//  BarsDemo is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  BarsDemo is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with KeyboardResponsiveDemo.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 
 import UIKit
 
